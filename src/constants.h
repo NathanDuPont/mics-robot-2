cabdb1b97a86f63be4b94166e7e706ba87cf2e84
NathanDuPont <nathan.dupont01@gmail.com>
2022-04-01 06:28:36 -0500

    Adding base functions to control robot 2

---
+++ src/constants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define COLOR_LED_PIN       (17)
+
+#define ROTATION_PWM_PIN    (36)
+#define ROTATION_IN1_PIN    (39)
+#define ROTATION_IN2_PIN    (38)
+
+#define DRIVER_PWM_PIN      (37)
+#define DRIVER_IN1_PIN      (40)
+#define DRIVER_IN2_PIN      (41)
+
+#define DEBUG               (0)
